a116ef1144e54330a812e536d5c14aaf9da1b704
Roly Fentanes <roly426@gmail.com>
2014-08-17 02:17:17 -0400

    support ALL generations!

--- js/main.js
+++ js/main.js
@@ -2,30 +2,119 @@
   var NEXT_ROUND_TIMEOUT = 600;
   var PING_TIMEOUT = 600;
 
-  // Construct paths for audio and sprites.
+  // For every generation, keep a copy of all pokemon and the pokemon
+  // that are left to be guessed from that generation.
   var gens = {
-    gen1: ['yellow', '/old'],
-    gen2: ['crystal', '/old'],
-    gen3: ['emerald', '/old'],
-    gen4: ['platinum', '/old'],
-    gen5: ['black-white', '/old'],
-    gen6: ['x-y', '']
+    1: {
+      enabled: true,
+      spritesDir: 'yellow',
+      criesDir: '/old',
+      allPokemon: window.pokemon.gen1.slice(),
+      pokemonLeft: window.pokemon.gen1.slice()
+    },
+    2: {
+      enabled: false,
+      spritesDir: 'crystal',
+      criesDir: '/old',
+      allPokemon: window.pokemon.gen2.slice(),
+      pokemonLeft: window.pokemon.gen2.slice()
+    },
+    3: {
+      enabled: false,
+      spritesDir: 'emerald',
+      criesDir: '/old',
+      allPokemon: window.pokemon.gen3.slice(),
+      pokemonLeft: window.pokemon.gen3.slice()
+    },
+    4: {
+      enabled: false,
+      spritesDir: 'platinum',
+      criesDir: '/old',
+      allPokemon: window.pokemon.gen4.slice(),
+      pokemonLeft: window.pokemon.gen4.slice()
+    },
+    5: {
+      enabled: false,
+      spritesDir: 'black-white',
+      criesDir: '/old',
+      allPokemon: window.pokemon.gen5.slice(),
+      pokemonLeft: window.pokemon.gen5.slice()
+    },
+    6: {
+      enabled: false,
+      spritesDir: 'x-y',
+      criesDir: '',
+      allPokemon: window.pokemon.gen6.slice(),
+      pokemonLeft: window.pokemon.gen6.slice()
+    }
   };
 
+  // Construct paths for audio and sprites.
   for (var gen in gens) {
     var d = gens[gen];
-    var pokemonGen = window.pokemon[gen];
-    for (var i = 0, len = pokemonGen.length; i < len; i++) {
-      var pkm = pokemonGen[i];
-      pkm.sprite = 'media/sprites/' + d[0] + '/' + pkm.species_id + '.png';
-      pkm.cry = 'media/cries' + d[1] + '/' + pkm.species_id + '.mp3';
+    for (var i = 0, len = d.allPokemon.length; i < len; i++) {
+      var pkm = d.allPokemon[i];
+      pkm.sprite =
+        'media/sprites/' + d.spritesDir + '/' + pkm.species_id + '.png';
+      pkm.cry =
+        'media/cries' + d.criesDir + '/' + pkm.species_id + '.mp3';
     }
   }
 
+  // Gets a list of lists of pokemon from all enabled generations.
+  function getPokemon(key) {
+    var all = [];
+    for (var gen in gens) {
+      var d = gens[gen];
+      if (d.enabled) {
+        all.push(d[key]);
+      }
+    }
+    return all;
+  }
+
+  function getAllPokemon() {
+    return getPokemon('allPokemon');
+  }
+
+  function getPokemonLeft() {
+    return getPokemon('pokemonLeft');
+  }
+
+  var allPokemon, pokemonLeft;
+
+  // This is called when the game first begins, and whenever
+  // the generations enabled is updated.
+  function updatePokemon() {
+    allPokemon = getAllPokemon();
+    pokemonLeft = getPokemonLeft();
+  }
+  updatePokemon();
+
+  // Gets a random pokemon from a list of lists, which will be either
+  // all pokemon from enabled generations, or all pokemon that are left.
+  function randomFromLists(lists, remove) {
+    var list = lists[~~(Math.random() * lists.length)];
+    var index = ~~(Math.random() * list.length);
+    var pkm = list[index];
+    if (remove) {
+      list.splice(index, 1);
+    }
+    return pkm;
+  }
+
+  // Returns true if there are any pokemon left to play.
+  function arePokemonLeft() {
+    if (!pokemonLeft.length) { return false; }
+    for (var i = 0, len = pokemonLeft.length; i < len; i++) {
+      if (pokemonLeft[i].length) { return true; }
+    }
+    return false;
+  }
+
+  // Keep track of player stats.
   var totalGuesses = 0;
   var correctGuesses = 0;
-  var allPokemons = window.pokemon.gen1;
-  var pokemonsLeft = allPokemons.slice();
   var guessedWrong = [];
 
   var $options = $('.options').children();
@@ -38,6 +127,18 @@
   $play.click(playCry);
   $play.jrumble();
 
+  $('.gen').click(function() {
+    var $gen = $(this);
+    $gen.toggleClass('enabled');
+    var gen = $gen.attr('data-gen');
+    gens[gen].enabled = $gen.hasClass('enabled');
+    updatePokemon();
+  });
+
+  $('.gen-1').addClass('enabled');
+
+  // Returns `n` pokemon that are not the given pokemon.
+  // Used to have them be shuffled in with the pokemon to be guessed.
   function randomPokemonThatAreNot(theid, n) {
     var pokemons = [];
     var pokemonsHash = {};
@@ -46,18 +147,13 @@
     for (var i = 0; i < n; i++) {
       var pokemon;
       do {
-        pokemon = allPokemons[~~(Math.random() * allPokemons.length)];
+        pokemon = randomFromLists(allPokemon, false);
       } while (pokemonsHash[pokemon.species_id] === true);
       pokemons.push(pokemon);
       pokemonsHash[pokemon.species_id] = true;
     }
 
     return pokemons;
-  }
-
-  function randomNonPastPokemon() {
-    var index = ~~(Math.random() * pokemonsLeft.length);
-    return pokemonsLeft.splice(index, 1)[0];
   }
 
   function shuffle(array) {
@@ -79,7 +175,7 @@
   var theCry;
 
   function nextRound() {
-    thePokemon = randomNonPastPokemon();
+    thePokemon = randomFromLists(pokemonLeft, true);
     var roundPokemons = randomPokemonThatAreNot(thePokemon.species_id, 3);
     roundPokemons.push(thePokemon);
     roundPokemons = shuffle(roundPokemons);
@@ -144,7 +240,7 @@
     $sprite.attr('src', src);
     $sprite.removeClass('hidden');
 
-    if (pokemonsLeft.length) {
+    if (arePokemonLeft()) {
       setTimeout(nextRound, NEXT_ROUND_TIMEOUT);
     } else {
       displayEndScreen();
